90b30d9dc24bf810fe2a6b676954f004f7fe287d
haren7474 <39461847+haren7474@users.noreply.github.com>
2020-05-25 11:24:45 +0530

    Added functionality: Add/ remove from middle

--- SingleLinkList.java
+++ SingleLinkList.java
@@ -2,60 +2,103 @@ package com.tyss.ds;
 
 public class SingleLinkList {
 
-	    class Node{    
-	        int data;    
-	        Node next;    
-	            
-	        public Node(int data) {    
-	            this.data = data;    
-	            this.next = null;    
-	        }    
-	    }    
-	      
-	    public Node head = null;    
-	    public Node tail = null;    
-	        
-	    
-	    public void addNode(int data) {    
-	        Node newNode = new Node(data);    
-	            
-	        if(head == null) {    
-	            head = newNode;    
-	            tail = newNode;    
-	        }    
-	        else {    
-	            tail.next = newNode;    
-	            tail = newNode;    
-	        }    
-	    }    
-	        
-	    public void display() {    
-	        Node current = head;    
-	            
-	        if(head == null) {    
-	            System.out.println("List is empty");    
-	            return;    
-	        }    
-	        System.out.println("My Single Link list: ");    
-	        while(current != null) {    
-	            System.out.print(current.data + " ");    
-	            current = current.next;    
-	        }    
-	        System.out.println();    
-	    }    
-	        
-	    public static void main(String[] args) {    
-	            
-	    	SingleLinkList ssl = new SingleLinkList();    
-	            
-	        ssl.addNode(10);    
-	        ssl.addNode(20);    
-	        ssl.addNode(30);    
-	        ssl.addNode(40);  
-	        ssl.addNode(50); 
-	            
-	        ssl.display();    
-	    }    
+	public class Node {
+		int data;
+		Node next;
+
+		public Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+
+	public Node head = null;
+	public Node tail = null;
+
+	public void addNode(int data) {
+		Node newNode = new Node(data);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+	}
+
+	public void display() {
+		Node current = head;
+
+		if (head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		System.out.println("\nMy Single Link list: ");
+		while (current != null) {
+			System.out.print(current.data + " ");
+			current = current.next;
+		}
+		System.out.println();
+	}
+
+	public void addInMiddle(Node newNode, Node checkNode) {
+		Node current = head;
+
+		if (head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		while (current != null) {
+			if (current.data == checkNode.data) {
+				newNode.next = current.next;
+				current.next = newNode;
+				System.out.print(newNode.data + " has been inserted\n");
+				return;
+			}
+			current = current.next;
+		}
+	}
 	
+	public void deleteInMiddle(Node nodeToDelete) {
+		Node current = head;
+
+		if (head == null) {
+			System.out.println("List is empty");
+			return;
+		}
+		while (current != null) {
+			if (current.next.data == nodeToDelete.data) {
+				current.next = nodeToDelete.next;
+				nodeToDelete.next = null;
+				System.out.print(nodeToDelete.data + " has been deleted\n");
+				return;
+			}
+			current = current.next;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		SingleLinkList ssl = new SingleLinkList();
+
+		ssl.addNode(10);
+		ssl.addNode(20);
+		ssl.addNode(30);
+		ssl.addNode(40);
+		ssl.addNode(50);
+
+		ssl.display();
+		Node newNode = ssl.new Node(80);
+		Node checkNode = ssl.new Node(20);
+		ssl.addInMiddle(newNode, checkNode);
+
+		ssl.display();
+		
+		ssl.deleteInMiddle(newNode);
+		
+		System.out.println("List after deletion: ");
+		ssl.display();
+	}
 
 }
